911e7604eb3ddddd16300470e7bf948b0828feb4
Vicente Arce <vicente.arce.p@usach.cl>
2024-10-31 22:20:28 -0300

    Modificando httpCommon

--- Frontend/src/http-common.js
+++ Frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const prestaBancoServer = import.meta.env.VITE_PRESTABANCO_BACKEND_SERVER;
-const prestaBancoPort = import.meta.env.VITE_PRESTABANCO_BACKEND_PORT;
+const prestaBancoServer = "localhost";
+const prestaBancoPort = "80";
 
 export default axios.create({
     baseURL: `http://${prestaBancoServer}:80`,
